4a507b4a5777029ed4824bb4c327521fceca7165
Peiying Wen <wenpeiying@gmail.com>
2013-07-19 12:07:54 -0500

    summarize untested classes/functions in README

--- src/com/twu/rover/Main.java
+++ src/com/twu/rover/Main.java
@@ -4,16 +4,17 @@ public class Main {
 
     public static void main(String args[]){
 
+        String fileName = "input";
         Queue<Rover> q = new Queue<Rover>();
         MyFileReader fileReader = new MyFileReader(q);
-        fileReader.read(args[0]);
+        fileReader.read(fileName);
 
         int size = q.size();
         for (int i=0; i<size; i++){
             Rover rover = q.dequeue();
             rover.move();
             Position pos = rover.getPosition();
-            System.out.printf("%d %d %c\n", pos.getX(), pos.getY(), rover.getHeading().toString());
+            System.out.printf("%d %d %s\n", pos.getX(), pos.getY(), rover.getHeading().toString());
         }
 
     }
